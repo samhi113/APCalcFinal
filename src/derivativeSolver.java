a3cab1ec9f8b774e67993b09de2ca7ca17777bb4
samhi113 <samhiggins113@gmail.com>
2022-04-29 13:26:10 +0000

    initial push

---
+++ src/derivativeSolver.java
@@ -0,0 +1,11 @@
+public class derivativeSolver {
+    /*
+    This program will:
+    1. ask for the full problem
+    2. then solve for the derivative
+    3. display solution
+     */
+    public static void main(String[] args) {
+
+    }
+}
